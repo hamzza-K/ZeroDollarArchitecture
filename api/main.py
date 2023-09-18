9038447f72430427d984aee6d533b9a9761777da
Hamzza K <hk151817@gmail.com>
2023-09-18 12:37:30 +0000

    api works

---
+++ api/main.py
@@ -0,0 +1,49 @@
+from typing import Any, Union
+from datetime import datetime
+from pydantic import BaseModel
+from xata.client import XataClient
+from fastapi import FastAPI, HTTPException, Query, Body
+
+app = FastAPI()
+
+API_KEY = "xau_rp52AzXJYDB0nQujNAvquPVOmlLiR4hj1"
+DB_URL = "https://Makafate-Amal-s-workspace-98ldke.us-east-1.xata.sh/db/zeroDollar"
+
+xata = XataClient(api_key=API_KEY, db_url=DB_URL)
+
+class FilterParams(BaseModel):
+    """Base Model class to pass into the FastAPI routes for xata client."""
+    table_name: str
+    columns: list
+    filter: Union[dict, None] = None
+    sort: Union[dict, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"message": "All OK"}
+
+@app.post("/read-records/")
+def read_records(filter_params: FilterParams):
+    """Reads the records from XataDB"""
+    try:
+        resp = xata.data().query(filter_params.table_name, {
+            "columns": filter_params.columns,
+            "filter": filter_params.filter,
+            "sort": filter_params.sort
+        })
+
+        if resp.is_success():
+            records = resp.json()["records"]
+
+            for record in records:
+                date_str = record["date"]
+                date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+                formatted_date = date_obj.strftime("%A, %B %d, %Y")
+                record["formatted_date"] = formatted_date
+
+            return records
+        else:
+            raise HTTPExecption(status_code=resp.status_code, detail="Error reading records")
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
