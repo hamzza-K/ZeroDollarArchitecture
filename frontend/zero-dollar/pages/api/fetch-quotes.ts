0b7869731ea853d096d48cc3a533de9e3ee6479e
Hamzza K <hk151817@gmail.com>
2023-09-20 14:20:29 +0000

    initial commit

--- frontend/zero-dollar/pages/api/fetch-quotes.ts
+++ frontend/zero-dollar/pages/api/fetch-quotes.ts
@@ -1,35 +1,11 @@
-import { startOfWeek, addWeeks, format } from "date-fns";
+import { addWeeks, format } from "date-fns";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { json } from "stream/consumers";
-
-
-
-interface Filter {
-    [key: string]: string[]
-}
-
-interface Sort {
-    [key: string]: string
-}
-
-type RequestData = {
-    table_name: string,
-    filter: Filter,
-    sort: Sort
-}
 
 type ResponseData = {
-    event_name: string,
-    event_location: string,
-    event_date: string,
-    xata: {
-        createdAt: string,
-        updatedAt: string,
-        version: number
-    },
-    formatted_date: string
+    date: string,
+    author: string,
+    qupte: string,
 }
-
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -39,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const formattedCurrentDate: string = format(currentDate, "yyyy-MM-dd'T'HH:mm:ss'Z'");
         const formattedNextWeek: string = format(nextWeek, "yyyy-MM-dd'T'HH:mm:ss'Z'");
 
-        const URL: string = "https://myapp-shtaldaidq-uc.a.run.app/read-records/"
+        const URL: string = "https://zero-dollar-api-rblfprys6q-ue.a.run.app/read-records/"
 
 
         const response = await fetch(URL, {
@@ -49,10 +25,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             },
             body: JSON.stringify({
                 table_name: "zero",
-                columns: ["date", "event_location", "event_description", "event_date", "event_link"],
-                filter: {"$all": [{"event_date": {"$ge": formattedCurrentDate}},
-                                  {"event_date": {"$lt": formattedNextWeek}}]},
-                sort: {"event_date": "asc"}
+                columns: ["date", "author", "quote"],
+                filter: {"$all": [{"date": {"$ge": formattedCurrentDate}},
+                                  {"date": {"$lt": formattedNextWeek}}]},
+                sort: {"date": "asc"}
             })
         });
         const data: Promise<ResponseData> = await response.json();
